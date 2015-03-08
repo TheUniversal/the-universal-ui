1114ed0150ded747eb8555e5ffafa297283f9fea
Matias Klemola <matias.klemola@gmail.com>
2015-03-08 17:43:05 +0200

    to5ify -> babelify, update babel version

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var to5ify = require('6to5ify');
+var babelify = require('babelify');
 var del = require('del');
 var source = require('vinyl-source-stream');
 var connect = require('connect');
@@ -14,7 +14,7 @@ gulp.task('clean', function(cb) {
 
 gulp.task('js', ['clean'], function() {
     browserify('./src/main.js')
-        .transform(to5ify)
+        .transform(babelify)
         .bundle()
         .pipe(source('main.js'))
         .pipe(gulp.dest('./dist'));
