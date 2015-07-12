6deb9128bb5459abfc08a4ae316ade268188f7ba
Matias Klemola <matias.klemola@gmail.com>
2015-07-12 16:53:38 +0300

    updated PlayPauseButton to use command and playback status constants, added the-universal-common dependency

--- src/components/button/PlayPauseButton.js
+++ src/components/button/PlayPauseButton.js
@@ -2,24 +2,27 @@
 
 var hg = require('mercury');
 var h = require('mercury').h;
+var PlaybackCommands = require('the-universal-common/command/Commands').PLAYBACK;
+var PlaybackStatus = require('the-universal-common/playback/PlaybackStatus');
 var socket = require('../../services/Socket');
 
 function PlayPauseButton() {
     var state = hg.state({
-        action: hg.value('Play'),
+        action: hg.value(PlaybackCommands.PLAY),
         playing: hg.value(false),
         channels: {
             playOrPause: (state) => {
-                socket.emit('playback', state.playing() ? 'pause' : 'play');
+                socket.emit('playback', state.playing() ?
+                    PlaybackCommands.PAUSE : PlaybackCommands.PLAY);
             }
         }
     });
 
-    socket.on('playback', function (command) {
-        console.log(command);
-        let playing = command === 'play';
+    socket.on('playback', function (playbackStatus) {
+        console.log(playbackStatus);
+        let playing = playbackStatus === PlaybackStatus.PLAYING;
         state.playing.set(playing);
-        state.action.set(playing ? 'Pause' : 'Play');
+        state.action.set(playing ? PlaybackCommands.PAUSE : PlaybackCommands.PLAY);
     });
 
     return state
